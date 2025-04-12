977dc526d16838d90f2138e022b61bb2c33d3f2e
Akash More <moreakash029@gmail.com>
2025-04-12 15:43:10 +0530

    video added in home page

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
   return (
     <section className="home image" id="home">
-
+      <video width="100%" height="100%" autoPlay
+  muted
+  loop
+  playsInline
+>
+        <source src="videos/webFeliciaWalkThrough.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
       {/* CALL TO ACTION STARTS */}
       <span className="animated-layer animated-btn cta" id="cta">
         <span></span>
